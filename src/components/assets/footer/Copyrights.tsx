75fd72fe2477ab62b0973c0c6e1adbff94d8b373
Mehmed Khan <smmehmedkhan@gmail.com>
2026-04-25 16:42:25 +0600

    fix: fixes combo-layout miss splitting issue, make hero-intro text center on mobile

--- src/components/assets/footer/Copyrights.tsx
+++ src/components/assets/footer/Copyrights.tsx
@@ -4,10 +4,13 @@ export default function Copyrights() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <div className="copyrights">
-      <Paragraph variant="muted" size="sm">
-        © {currentYear} Mehmed Khan. All rights reserved.
-      </Paragraph>
+    <div className="mt-auto w-full">
+      <hr className="w-full border-border" />
+      <div className="copyrights">
+        <Paragraph variant="muted" size="sm">
+          © {currentYear} Mehmed Khan. All rights reserved.
+        </Paragraph>
+      </div>
     </div>
   )
 }
--- src/components/partials/Footer.tsx
+++ src/components/partials/Footer.tsx
@@ -6,26 +6,27 @@ import Resources from '@/components/assets/footer/Resources'
 
 export default function Footer() {
   return (
-    <footer className="wrapper">
-      <div className="container footer-grid">
-        {/* addresses: left on lg */}
-        <div className="2xl:order-1">
-          <Addresses />
-        </div>
+    <footer>
+      <div className="container flex-1 flex items-center justify-center">
+        <div className="footer-grid">
+          {/* addresses: left on lg */}
+          <div className="2xl:order-1">
+            <Addresses />
+          </div>
 
-        {/* newsletter: top on md, centered on sm/lg */}
-        <div className="mx-auto md:col-span-2 md:order-first 2xl:order-2 2xl:col-span-1">
-          <Newsletters />
-        </div>
+          {/* newsletter: top on md, centered on sm/lg */}
+          <div className="mx-auto md:col-span-2 md:order-first 2xl:order-2 2xl:col-span-1">
+            <Newsletters />
+          </div>
 
-        {/* resources: right on lg */}
-        <div className="2xl:order-3">
-          <Suspense>
-            <Resources />
-          </Suspense>
+          {/* resources: right on lg */}
+          <div className="2xl:order-3">
+            <Suspense>
+              <Resources />
+            </Suspense>
+          </div>
         </div>
       </div>
-      <hr className="w-full border-border" />
       <Copyrights />
     </footer>
   )
